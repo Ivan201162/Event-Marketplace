45392e6d230266e5c32df7f4ac5afd88a9656d56
Ivan201162 <ivan.bliz279@gmail.com>
2025-09-19 16:29:14 +0700

    fix(E9): добавлен riverpod_annotation и создан riverpod_compat

---
+++ lib/core/riverpod/riverpod_compat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Совместимость с классическими типами Riverpod
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Тип для Reader функции
+typedef Reader = T Function<T>(ProviderListenable<T> provider);
+
+/// Расширение для Ref с синхронным чтением
+extension RefX on Ref {
+  /// Синхронное чтение провайдера
+  T readSync<T>(ProviderListenable<T> provider) {
+    return read(provider);
+  }
+}
